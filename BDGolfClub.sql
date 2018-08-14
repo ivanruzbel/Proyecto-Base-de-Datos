4cff78217dade386f7f9ae2d6d0b1957b3b47e5c
ivanruzbel <38087641+ivanruzbel@users.noreply.github.com>
2018-08-13 23:24:02 -0400

    Update BDGolfClub.sql

--- BDGolfClub.sql
+++ BDGolfClub.sql
@@ -1,98 +1,104 @@
-﻿/****** PC-X120E IVAN FELIZ******/
- /****** Ivan Ruzbel	Feliz Romero 16-MIIN-1-178 Seccion 0541******/
 
-CREATE DATABASE [DBGolfClub]
-  
-GO
-USE [DBGolfClub]
-GO
 
-CREATE TABLE [dbo].[membrecias](
-	[Membrecia_id] [int] NOT NULL,
-	[Cedula] [varchar](20) NULL,
-	[Membrecia_tipo] [varchar](10) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Membrecia_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+/****** Ivan Ruzbel Feliz Romero Matrícula 16-MIIN-1-178 SECCIÓN 0541 ******/CREATE DATABASE [DBClubGolf]
+
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DBClubGolf', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.IVANPC\MSSQL\DATA\DBClubGolf.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'DBClubGolf_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.IVANPC\MSSQL\DATA\DBClubGolf_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+ALTER DATABASE [DBClubGolf] SET COMPATIBILITY_LEVEL = 120
 
 GO
-SET ANSI_PADDING OFF
+USE [DBClubGolf]
 GO
-/****** Object:  Table [dbo].[miembros_Info]    Script Date: 8/4/2018 12:39:45 PM ******/
+/****** Object:  Table [dbo].[Info_miembros]    Script Date: 8/13/2018 11:01:05 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-SET ANSI_PADDING ON
-GO
-CREATE TABLE [dbo].[miembros_Info](
-	[Cedula] [varchar](20) NOT NULL,
-	[Nombre] [varchar](20) NOT NULL,
-	[telefono] [varchar](50) NULL,
-PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Info_miembros](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nvarchar](100) NULL,
+	[Telefono] [int] NULL,
+ CONSTRAINT [PK_Info_miembros] PRIMARY KEY CLUSTERED 
 (
-	[Cedula] ASC
+	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[Pagos]    Script Date: 8/4/2018 12:39:45 PM ******/
+/****** Object:  Table [dbo].[Membrecias]    Script Date: 8/13/2018 11:01:05 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-SET ANSI_PADDING ON
+CREATE TABLE [dbo].[Membrecias](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Tipo_Membrecia] [nchar](10) NULL,
+ CONSTRAINT [PK_Membrecias] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Pagos]    Script Date: 8/13/2018 11:01:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Pagos](
-	[Pago_id] [int] NOT NULL,
-	[Membrecia_id] [int] NULL,
-	[Nombre] [varchar](20) NULL
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Pago] [int] NULL,
+ CONSTRAINT [PK_Pagos] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (1, N'00000000001', N'GOLD')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (2, N'00000000002', N'NORMAL')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (3, N'00000000003', N'BASE')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (4, N'00000000004', N'GOLD')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (5, N'00000000005', N'NORMAL')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (6, N'00000000006', N'GOLD')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (7, N'00000000007', N'NORMAL')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (8, N'00000000008', N'BASE')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (9, N'00000000009', N'NORMAL')
-INSERT [dbo].[membrecias] ([Membrecia_id], [Cedula], [Membrecia_tipo]) VALUES (10, N'00000000010', N'GOLD')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000001', N'Juan Ayala', N'800111111')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000002', N'Juana Mena', N'0021112222')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000003', N'karlos Matos', N'0010092212')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000004', N'Loren Rijo', N'0092332232')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000005', N'Jose Perez', N'0897888888')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000006', N'Julio Mora', N'9894433322')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000007', N'Lenon Mota', N'8932338844')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000008', N'Hansel Lora', N'8943221888')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000009', N'Lina Mirian', N'8932121883')
-INSERT [dbo].[miembros_Info] ([Cedula], [Nombre], [telefono]) VALUES (N'00000000010', N'Kendry Lon', N'8923884444')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (1, 1, N'Juan Ayala')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (2, 2, N'Juana Mena')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (3, 3, N'karlos Matos')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (4, 4, N'Loren Rijo')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (5, 5, N'Jose Perez')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (6, 6, N'Julio Mora')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (7, 7, N'Lenon Mota')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (8, 8, N'Hansel Lora')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (9, 9, N'Lina Mirian')
-INSERT [dbo].[Pagos] ([Pago_id], [Membrecia_id], [Nombre]) VALUES (10, 10, NULL)
-ALTER TABLE [dbo].[membrecias]  WITH CHECK ADD FOREIGN KEY([Cedula])
-REFERENCES [dbo].[miembros_Info] ([Cedula])
-GO
-ALTER TABLE [dbo].[Pagos]  WITH CHECK ADD FOREIGN KEY([Membrecia_id])
-REFERENCES [dbo].[membrecias] ([Membrecia_id])
-GO
+SET IDENTITY_INSERT [dbo].[Info_miembros] ON 
+
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (1, N'Juan Ayala', NULL)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (2, N'Juana Mena', 12345)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (3, N'Karlos Matos', 1235456)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (4, N'Loren Rijo', 234356)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (5, N'Jose Perez', 46556775)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (6, N'Julio Mora', 45653422)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (7, N'Lenon Mota', 4534634)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (8, N'Hansel Lora', 36345232)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (9, N'Lina Mirian', 235456433)
+INSERT [dbo].[Info_miembros] ([ID], [Nombre], [Telefono]) VALUES (10, N'Kendry Lon', 242354235)
+SET IDENTITY_INSERT [dbo].[Info_miembros] OFF
+SET IDENTITY_INSERT [dbo].[Membrecias] ON 
+
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (1, N'GOLD      ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (2, N'NORMAL    ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (3, N'Diamante  ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (4, N'GOLD      ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (5, N'NORMAL    ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (6, N'GOLD      ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (7, N'NUEVA     ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (8, N'Antigua   ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (9, N'Moderna   ')
+INSERT [dbo].[Membrecias] ([ID], [Tipo_Membrecia]) VALUES (10, N'MEGA      ')
+SET IDENTITY_INSERT [dbo].[Membrecias] OFF
+SET IDENTITY_INSERT [dbo].[Pagos] ON 
+
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (1, 100)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (2, 200)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (3, 300)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (4, 400)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (5, 500)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (6, 600)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (7, 700)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (8, 800)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (9, 900)
+INSERT [dbo].[Pagos] ([ID], [Pago]) VALUES (10, 10000)
+SET IDENTITY_INSERT [dbo].[Pagos] OFF
 USE [master]
 GO
-ALTER DATABASE [DBGolfClub] SET  READ_WRITE 
+ALTER DATABASE [DBClubGolf] SET  READ_WRITE 
 GO
