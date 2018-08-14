44fdc6a31533ff635a23f18a3fb883b39d7414b2
ivanruzbel <38087641+ivanruzbel@users.noreply.github.com>
2018-08-13 23:25:05 -0400

    Update DBBodega.sql

--- DBBodega.sql
+++ DBBodega.sql
@@ -1,101 +1,112 @@
-﻿
-/****** PC-X120E IVAN FELIZ******/ 
-/****** Ivan Ruzbel	Feliz Romero 16-MIIN-1-178 Seccion 0541******/
 
-CREATE DATABASE [DBBodega]
- 
-GO
-USE [DBBodega]
-GO
+/****** Ivan Ruzbel Feliz Romero Matrícula 16-MIIN-1-178 SECCIÓN 0541 ******/
 
-CREATE TABLE [dbo].[articulos](
-	[ArticuloID] [int] NOT NULL,
-	[Nombre_Articulo] [varchar](20) NULL,
+CREATE DATABASE [DBbodega]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DBbodega', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.IVANPC\MSSQL\DATA\DBbodega.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'DBbodega_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.IVANPC\MSSQL\DATA\DBbodega_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+ALTER DATABASE [DBbodega] SET COMPATIBILITY_LEVEL = 120
+
+GO
+USE [DBbodega]
+GO
+/****** Object:  Table [dbo].[Articulos]    Script Date: 8/13/2018 10:20:20 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Articulos](
+	[ID_Articulo] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre_Articulo] [nvarchar](100) NULL,
 	[Cantidad] [int] NULL,
 	[Precio] [int] NULL,
-PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Articulos] PRIMARY KEY CLUSTERED 
 (
-	[ArticuloID] ASC
+	[ID_Articulo] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[ordenes]    Script Date: 8/4/2018 1:09:20 PM ******/
+/****** Object:  Table [dbo].[Ordenes]    Script Date: 8/13/2018 10:20:20 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-SET ANSI_PADDING ON
-GO
-CREATE TABLE [dbo].[ordenes](
-	[Orden_id] [int] NOT NULL,
-	[Articulo_id] [int] NULL,
-	[Articulo] [varchar](20) NULL,
-	[Cantidad] [int] NULL
-) ON [PRIMARY]
-
-GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[suplidor]    Script Date: 8/4/2018 1:09:20 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
-CREATE TABLE [dbo].[suplidor](
-	[Suplidor_id] [int] NOT NULL,
-	[Articulo_id] [int] NULL,
-	[Nombre] [varchar](10) NULL,
-PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Ordenes](
+	[ID_Articulo] [int] IDENTITY(1,1) NOT NULL,
+	[Articulo] [nvarchar](100) NULL,
+	[Cantidad] [int] NULL,
+ CONSTRAINT [PK_Ordenes] PRIMARY KEY CLUSTERED 
 (
-	[Suplidor_id] ASC
+	[ID_Articulo] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
+/****** Object:  Table [dbo].[Suplidores]    Script Date: 8/13/2018 10:20:20 PM ******/
+SET ANSI_NULLS ON
 GO
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (1, N'Coca Cola', 600, 50)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (2, N'Pepsi', 5, 100)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (3, N'7up', 500, 30)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (4, N'Rica Naranja', 400, 32)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (5, N'Rica Pera', 300, 25)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (6, N'Rica Manzana', 200, 30)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (7, N'Cerveza Grande', 400, 40)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (8, N'Cerveza Normal', 500, 50)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (9, N'Malta Alemana', 2, 34)
-INSERT [dbo].[articulos] ([ArticuloID], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (10, N'Malta Morena', 3, 56)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (1, 1, N'Coca Cola', 10)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (2, 2, N'Pepsi', 20)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (3, 3, N'7up', 30)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (4, 4, N'Rica Naranja', 40)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (5, 5, N'Rica Pera', 50)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (6, 6, N'Rica Manzana', 60)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (7, 7, N'Cerveza Grande', 70)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (8, 8, N'Cerveza Normal', 80)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (9, 9, N'Malta Alemana', 90)
-INSERT [dbo].[ordenes] ([Orden_id], [Articulo_id], [Articulo], [Cantidad]) VALUES (10, 10, N'Malta Morena', 100)
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (1, 1, N'Bepensa')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (2, 2, N'Pepsi')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (3, 3, N'Bepensa')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (4, 4, N'Rica')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (5, 5, N'Rica')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (6, 6, N'Rica')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (7, 7, N'Cerveceria')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (8, 8, N'Cerveceria')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (9, 9, N'Cerveceria')
-INSERT [dbo].[suplidor] ([Suplidor_id], [Articulo_id], [Nombre]) VALUES (10, 10, N'Cerveceria')
-ALTER TABLE [dbo].[ordenes]  WITH CHECK ADD FOREIGN KEY([Articulo_id])
-REFERENCES [dbo].[articulos] ([ArticuloID])
+SET QUOTED_IDENTIFIER ON
 GO
-ALTER TABLE [dbo].[suplidor]  WITH CHECK ADD FOREIGN KEY([Articulo_id])
-REFERENCES [dbo].[articulos] ([ArticuloID])
+CREATE TABLE [dbo].[Suplidores](
+	[ID_Articulo] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nvarchar](100) NULL,
+	[Suplidor] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Suplidores] PRIMARY KEY CLUSTERED 
+(
+	[ID_Articulo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET IDENTITY_INSERT [dbo].[Articulos] ON 
+
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (1, N'Coca Cola', 600, 50)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (2, N'Pepsi', 5, 100)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (3, N'7up', 500, 30)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (4, N'Rica Naranja', 400, 32)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (5, N'Rica Pera', 300, 25)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (6, N'Rica Manzana', 200, 30)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (7, N'Cerveza Grande', 400, 40)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (8, N'Cerveza Normal', 500, 50)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (9, N'Malta Alemana', 2, 34)
+INSERT [dbo].[Articulos] ([ID_Articulo], [Nombre_Articulo], [Cantidad], [Precio]) VALUES (10, N'Malta Morena', 3, 56)
+SET IDENTITY_INSERT [dbo].[Articulos] OFF
+SET IDENTITY_INSERT [dbo].[Ordenes] ON 
+
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (1, N'Coca Cola', 29)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (2, N'Pepsi', 10)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (3, N'7up', 20)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (4, N'Rica Naranja', 30)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (5, N'Rica Pera', 40)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (6, N'Rica Manzana', 506)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (7, N'Cerveza Grande', 60)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (8, N'Cerveza Normal', 70)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (9, N'Malta Alemana', 80)
+INSERT [dbo].[Ordenes] ([ID_Articulo], [Articulo], [Cantidad]) VALUES (10, N'Malta Morena', 90)
+SET IDENTITY_INSERT [dbo].[Ordenes] OFF
+SET IDENTITY_INSERT [dbo].[Suplidores] ON 
+
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (1, N'Coca Cola', N'Bepensa')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (2, N'Pepsi ', N'Pepsi')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (3, N'7up', N'Bepensa')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (4, N'Rica Naranja', N'Rica')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (5, N'Rica Pera', N'Rica')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (6, N'Rica Manzana', N'Rica')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (7, N'Cerveza Grande', N'Cerveceria')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (8, N'Cerveza Normal', N'Cerveceria')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (9, N'Malta Alemana', N'Cerveceria')
+INSERT [dbo].[Suplidores] ([ID_Articulo], [Nombre], [Suplidor]) VALUES (10, N'Malta Morena', N'Cerveceria')
+SET IDENTITY_INSERT [dbo].[Suplidores] OFF
+ALTER TABLE [dbo].[Articulos]  WITH CHECK ADD  CONSTRAINT [FK_Articulos_Ordenes] FOREIGN KEY([ID_Articulo])
+REFERENCES [dbo].[Ordenes] ([ID_Articulo])
+GO
+ALTER TABLE [dbo].[Articulos] CHECK CONSTRAINT [FK_Articulos_Ordenes]
 GO
 USE [master]
 GO
-ALTER DATABASE [DBBodega] SET  READ_WRITE 
+ALTER DATABASE [DBbodega] SET  READ_WRITE 
 GO
